72fc1f84f814d1c783d42e94be72cef415d358d0
jpssoares <jps.soares@campus.fct.unl.pt>
2024-05-06 22:46:17 +0100

    Formated some code

--- src/components/DaisyHero.js
+++ src/components/DaisyHero.js
@@ -11,14 +11,14 @@ export const DaisyHero = () => {
             <Hero>
             <Hero.Content className="text-center">
                 <div className="max-w-md">
-                <h1 className="text-5xl font-bold">Hello there</h1>
-                <p className="py-6">
-                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-                    excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-                    a id nisi.
-                </p>
+                    <h1 className="text-5xl font-bold">Hello there</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+                        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+                        a id nisi.
+                    </p>
 
-                <Button color="primary">Get Started</Button>
+                    <Button color="primary">Get Started</Button>
                 </div>
             </Hero.Content>
             </Hero>
